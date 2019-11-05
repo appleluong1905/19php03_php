4f00da563ef6d712fd995fd0509b8bb506dda24c
Chad Luong <apple.luong1905@gmail.com>
2019-11-05 20:24:50 +0700

    update

---
+++ session4/register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+	<?php 
+		// connect php vs mysql
+		$server = "localhost";
+		$username = "root";
+		$password = "none"; // $password = "";
+		$database = "19php03";
+
+		// ket noi
+		$connect = mysqli_connect($server, $username, $password, $database);
+
+		// Check connection
+		if (mysqli_connect_errno())
+	  {
+	  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  }
+
+		if (isset($_POST['register'])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+
+			// cau lenh chen user vao db
+			$sql = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
+			
+			// thuc thi cau lenh sql
+			mysqli_query($connect, $sql);
+
+		}
+	?>
+	<h1>Register form</h1>
+	<form action="#" method="POST">
+		<p>
+			Username <input type="text" name="username">
+		</p>
+		<p>
+			Password <input type="password" name="password">
+		</p>
+		<p>
+			<input type="submit" name="register" value="Register">
+		</p>
+	</form>
+</body>
+</html>
