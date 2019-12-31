d94a8918a69946fd4822037cbbdea262ffbb9242
Chad Luong <apple.luong1905@gmail.com>
2019-12-31 19:00:58 +0700

    update

--- session12/controller/controller.php
+++ session12/controller/controller.php
@@ -1,9 +1,25 @@
 <?php 
+	include 'model/model.php';
 	class Controller {
 
 		function handleRequest(){
 			$action = isset($_GET['action'])?$_GET['action']:'home';
-			echo $action;
+			$model = new Model;
+			switch ($action) {
+				case 'home':
+					// lay thong tin trang chu ra
+					$homeData = $model->getHomepage();
+					include 'view/homepage.php';
+					break;
+				case 'about':
+					// lay thong tin trang about ra
+					$aboutData = $model->getAbout();
+					include 'view/about.php';
+					break;
+				default:
+					# code...
+					break;
+			}
 		}
 	}
 ?>
---
+++ session12/model/model.php
@@ -0,0 +1,14 @@
+<?php 
+	class Model {
+
+		function getHomepage() {
+			$homepageData = "Thong tin trang chu tai day";
+			return $homepageData;
+		}
+
+		function getAbout() {
+			$aboutData = "Thong tin trang about";
+			return $aboutData;
+		}
+	}
+?>
